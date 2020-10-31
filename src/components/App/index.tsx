8c5fa9a1fc4fea92377a391a904c5a2ae6947cbc
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-10-31 22:52:40 +0200

    done create Form

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, Form } from "formik";
+import { Formik, Form, Field } from "formik";
 import "./App.css";
 
 interface FormValues {
@@ -20,7 +20,31 @@ const App: React.FC = () => {
     <div className="App">
       <h1>Sign Up</h1>
       <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-        
+        {(props) => {
+          return (
+            <Form>
+              <div>
+                <label>Name: </label>
+                <Field autoComplete="off" name="name" as="input"></Field>
+              </div>
+              <div>
+                <label>Position: </label>
+                <Field
+                  autoComplete="off"
+                  name="position"
+                  placeholder="choose your position"
+                  as="select"
+                >
+                  <option value="front-end">Front End</option>
+                  <option value="back-end">Back End</option>
+                  <option value="ful-stack">Ful Stack</option>
+                  <option value="mern-stack">MERN stack</option>
+                </Field>
+              </div>
+              <button type="submit">Submit</button>
+            </Form>
+          );
+        }}
       </Formik>
     </div>
   );
