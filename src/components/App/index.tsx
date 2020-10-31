bf39179e441f2f9123a1d1345c34edfa1062efab
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-10-31 22:41:18 +0200

    create Formik schema

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Formik, Form } from "formik";
 import "./App.css";
+
+interface FormValues {
+  name: string;
+  position: string;
+}
+
+const initialValues: FormValues = {
+  name: "",
+  position: "",
+};
+
 const App: React.FC = () => {
+  const handleSubmit = (values: FormValues): void => {
+    alert(JSON.stringify(values));
+  };
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Sign Up</h1>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        
+      </Formik>
     </div>
   );
 };
